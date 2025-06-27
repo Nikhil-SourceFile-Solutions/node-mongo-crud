f382d4f69b39f80b3cb733d7dbc949589f49fd32
Nikhil Sasikumar <nikhil@sourcefilesolutions.com>
2025-06-27 15:18:09 +0530

    updated

--- server.js
+++ server.js
@@ -12,10 +12,17 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err.message);
+});
 // Routes
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
